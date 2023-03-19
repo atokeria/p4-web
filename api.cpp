a45073876320f214dcdbb2a82d74d96f1edd26a4
Ayush Tokeria <atokeria@umich.edu>
2023-03-18 20:46:47 -0400

    start api

--- api.cpp
+++ api.cpp
@@ -1,8 +1,18 @@
 // Project UID c1f28c309e55405daf00c565d57ff9ad
 
 #include <iostream>
+#include <list>
 using namespace std;
 
+class ohQueue {
+  private:
+    struct Student
+    {
+        /* data */
+    };
+
+    list<Student> queue;
+};
 
 int main() {
   
